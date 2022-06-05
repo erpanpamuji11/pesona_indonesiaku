54de4ee374e1de1e1fcce23bfdd007b1d615d683
Irfan Ardiansyah <57976165+Irfanard7@users.noreply.github.com>
2022-06-05 14:04:28 +0700

    Update list_wisata_page.dart

--- lib/presentation/wisata/list_wisata_page.dart
+++ lib/presentation/wisata/list_wisata_page.dart
@@ -12,7 +12,143 @@ class ListWisataPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text("Wisata Favoritmu"),
       ),
-      body: Container(),
+      body: Container(
+        margin: const EdgeInsets.all(15),
+        child: FutureBuilder<QuerySnapshot>(
+          future: DatabaseServices().firestoreRef.collection('wisata').get(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            } else if (snapshot.hasData && snapshot.data!.docs.isNotEmpty) {
+              List<DocumentSnapshot> arrData = snapshot.data!.docs;
+              print(arrData.length);
+              return GridView.builder(
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 20.0,
+                  crossAxisSpacing: 20.0,
+                  childAspectRatio: 0.75,
+                ),
+                itemCount: arrData.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Stack(
+                      children: [
+                        Container(
+                          height: 400,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black12,
+                                offset: Offset(
+                                  3.0,
+                                  3.0,
+                                ),
+                                blurRadius: 5.0,
+                                spreadRadius: 1.0,
+                              ), //BoxShadow
+                            ],
+                          ),
+                        ),
+                        Container(
+                          height: 400,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(10.0)),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Expanded(
+                                child: Container(
+                                  height: 200,
+                                  width: double.maxFinite,
+                                  decoration: BoxDecoration(
+                                      color: Colors.lightBlue,
+                                      borderRadius:
+                                          BorderRadius.circular(10.0)),
+                                  child: Image.network(
+                                    arrData[index]['imgUrl'],
+                                    fit: BoxFit.cover,
+                                    scale: 1.0,
+                                    loadingBuilder:
+                                        (context, child, loadingProgress) {
+                                      if (loadingProgress == null) {
+                                        return child;
+                                      } else {
+                                        return Center(
+                                          child: CircularProgressIndicator(
+                                            value: loadingProgress
+                                                        .expectedTotalBytes !=
+                                                    null
+                                                ? loadingProgress
+                                                        .cumulativeBytesLoaded /
+                                                    loadingProgress
+                                                        .expectedTotalBytes!
+                                                : null,
+                                          ),
+                                        );
+                                      }
+                                    },
+                                  ),
+                                ),
+                              ),
+                              Container(
+                                margin: const EdgeInsets.only(
+                                    left: 10, bottom: 10, top: 2),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Text(
+                                      arrData[index]['name'],
+                                      style: const TextStyle(
+                                          color: Colors.black,
+                                          fontSize: 15,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(
+                                      height: 4,
+                                    ),
+                                    Row(
+                                      children: [
+                                        const Icon(
+                                          Icons.location_on_rounded,
+                                          size: 12,
+                                          color: Colors.lightBlue,
+                                        ),
+                                        const SizedBox(
+                                          width: 5,
+                                        ),
+                                        Text(
+                                          arrData[index]['provincy'],
+                                          style: const TextStyle(
+                                              fontSize: 14,
+                                              fontWeight: FontWeight.w300),
+                                        )
+                                      ],
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              );
+            } else {
+              return const Center(
+                child: Text('Data not found'),
+              );
+            }
+          },
+        ),
+      ),
     );
   }
 }
